c8346d2d29959aaf00c3e7b1e285c4bcfb9812eb
sumitpatel <psumit88666@gmail.com>
2018-07-14 17:46:38 +0530

    demo
    demo

---
+++ app/build/generated/source/buildConfig/debug/com/skyzone18/demo1stchoise/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.skyzone18.demo1stchoise;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.skyzone18.demo1stchoise";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
